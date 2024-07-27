2a6bf1e237b513532360f112b39de13eb69f1136
HallamSaunders <hals4072@gmail.com>
2024-07-27 16:58:41 +0100

    Started overhaul.

---
+++ study-companion/app/auth/authscreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function authscreen() {
+  return (
+    <View>
+      <Text>authscreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
