71d18874d60e72540f7d6edd501c0a0aac9407e6
HallamSaunders <hals4072@gmail.com>
2024-07-28 11:44:21 +0100

    Added functionality for users to be added to Firestore database, to store data.

--- study-companion/app/auth/authscreen.tsx
+++ study-companion/app/auth/authscreen.tsx
@@ -2,9 +2,11 @@ import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 import { NavigationProp } from '@react-navigation/native';
 
-//Authentication
+//Authentication and firestore
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { FIREBASE_AUTH } from '../../firebase/firebase-config';
+import { FIREBASE_AUTH, FIRESTORE_DB } from '../../firebase/firebase-config';
+import { Timestamp } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
 
 //Color schemes
 import Colors from '../../constants/Colors';
@@ -14,6 +16,14 @@ interface RouterProps {
     navigation: NavigationProp<any, any>;
 }
   
+interface User {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    uid: string;
+    createdAt: Timestamp;
+}
 
 export default function AuthScreen({ navigation }: RouterProps) {
     //Color schemes
@@ -21,24 +31,54 @@ export default function AuthScreen({ navigation }: RouterProps) {
     const themeColors = colorScheme === 'dark' ? Colors.dark : Colors.light;
     
     //Variables storing parameters to use
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
+    //const isEmailValid: boolean = (email == null) ? false : email.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
     const [password, setPassword] = useState('');
     const [passwordConf, setPasswordConf] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [loading, setLoading] = useState(false);
   
-    const signUp = async () => {
+    const signUp = async (
+        username: string,
+        email: string,
+        password: string,
+        passwordConf: string,
+        firstName: string,
+        lastName: string
+    ) => {
       setLoading(true);
       if (password == passwordConf) {
         try {
-          const response = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
-          console.log(response);
+            const userCredential = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
+            console.log(userCredential);
+            const user = userCredential.user;
+            
+            if (user) {
+                //If a user exists, write the data to it
+                const userDoc: User = {
+                    username,
+                    email,
+                    firstName,
+                    lastName,
+                    uid: user.uid,
+                    createdAt: Timestamp.now()
+                };
+
+                try {
+                    //Write the created user to the Firestore database
+                    const docRef = await addDoc(collection(FIRESTORE_DB, 'users'), userDoc);
+                    console.log("Document written with ID: ", docRef.id);
+                } catch (error) {
+                    console.error("Error adding document: ", error);
+                }
+            }
         } catch (error) {
           console.log(error);
         } finally {
           setLoading(false);
         }
-      } else {
-        alert('Passwords do not match!');
       }
     }
 
@@ -50,22 +90,69 @@ export default function AuthScreen({ navigation }: RouterProps) {
                 color: themeColors.text,
                 marginBottom: 40
             }}>Sign Up</Text>
-            <TextInput value={email} placeholder='Email' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setEmail(text)}
+            <TextInput value={username} placeholder='Username' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setUsername(text)}
                 style={{
                     width: '80%',
                     height: 40,
-                    borderColor: themeColors.border,
+                    borderColor: (username === '') ? themeColors.borderAlert : themeColors.border,
                     borderWidth: 1,
                     borderRadius: 8,
                     paddingHorizontal: 10,
                     marginBottom: 12,
                     color: themeColors.text
                 }}></TextInput>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    width: '80%',
+                    marginBottom: 12,
+                    height: 40,
+                }}>
+                <TextInput value={firstName} placeholder='First Name' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setFirstName(text)}
+                    style={{
+                        height: '100%',
+                        borderColor: (firstName === '') ? themeColors.borderAlert : themeColors.border,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        paddingHorizontal: 10,
+                        color: themeColors.text,
+                        flex: 1,
+                        alignSelf: 'flex-start',
+                        marginRight: 12
+                    }}></TextInput>
+                <TextInput value={lastName} placeholder='Last Name' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setLastName(text)}
+                    style={{
+                        height: '100%',
+                        borderColor: (lastName === '') ? themeColors.borderAlert : themeColors.border,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        paddingHorizontal: 10,
+                        color: themeColors.text,
+                        flex: 1,
+                        alignSelf: 'flex-end'
+                    }}></TextInput>
+            </View>
+            <TextInput value={email} placeholder='Email' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setEmail(text)}
+                style={{
+                    width: '80%',
+                    height: 40,
+                    borderColor: (email === '') ? themeColors.borderAlert : themeColors.border,
+                    borderWidth: 1,
+                    borderRadius: 8,
+                    paddingHorizontal: 10,
+                    marginBottom: (!email.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') && !(email === '')) ? 2 : 12,
+                    color: themeColors.text
+                }}></TextInput>
+            {(!email.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') && !(email === '')) ? (
+                <Text style={{ marginBottom: 12 }}>Please enter a valid email.</Text>
+            ) : (
+                <View></View>
+            )}
             <TextInput value={password} placeholder='Password' placeholderTextColor={themeColors.subtleText} autoCapitalize='none' onChangeText={(text) => setPassword(text)} secureTextEntry={true}
                 style={{
                     width: '80%',
                     height: 40,
-                    borderColor: themeColors.border,
+                    borderColor: (password === '') ? themeColors.borderAlert : themeColors.border,
                     borderWidth: 1,
                     borderRadius: 8,
                     paddingHorizontal: 10,
@@ -76,14 +163,19 @@ export default function AuthScreen({ navigation }: RouterProps) {
                 style={{
                     width: '80%',
                     height: 40,
-                    borderColor: themeColors.border,
+                    borderColor: (passwordConf === '') ? themeColors.borderAlert : themeColors.border,
                     borderWidth: 1,
                     borderRadius: 8,
                     paddingHorizontal: 10,
-                    marginBottom: 12,
+                    marginBottom: (!(password === passwordConf)) ? 2 : 12,
                     color: themeColors.text
                 }}></TextInput>
-            <Pressable onPress={signUp}
+            {!(password === passwordConf) ? (
+                <Text style={{ marginBottom: 12 }}>Please ensure passwords match.</Text>
+            ) : (
+                <View></View>
+            )}
+            <Pressable onPress={() => signUp(username, email, password, passwordConf, firstName, lastName)}
                 style={{
                     width: '40%',
                     height: 40,
--- study-companion/firebase/firebase-config.ts
+++ study-companion/firebase/firebase-config.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_APIKEY,
@@ -20,3 +23,5 @@ export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
+
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
