00ed6d83314c8499704655e3fd4c22a4d00f5d37
HallamSaunders <hals4072@gmail.com>
2024-08-10 17:17:51 +0100

    Completed stopwatch mode.

--- study-companion/components/StopwatchTimer.tsx
+++ study-companion/components/StopwatchTimer.tsx
@@ -13,34 +13,42 @@ const StopwatchTimer = () => {
     //Timers
     const [stopwatchTime, setStopwatchTime] = useState(0);
     const [running, setRunning] = useState(false);
+    const [paused, setPaused] = useState(false);
+    const [stopped, setStopped] = useState(true);
     const timerRef = useRef<NodeJS.Timeout | null>(null);
-  
+
     const startStopwatch = () => {
-        if (!running) {
-            setRunning(true);
+        if (stopped || paused) {
             timerRef.current = setInterval(() => {
                 setStopwatchTime(prevTime => prevTime + 1);
             }, 1000);
         }
+        setRunning(true);
+        setPaused(false);
+        setStopped(false);
     };
-  
-    const stopStopwatch = () => {
+
+    const pauseStopwatch = () => {
         if (running && timerRef.current) {
-            setRunning(false);
             clearInterval(timerRef.current);
             timerRef.current = null;
         }
+        setRunning(false);
+        setPaused(true);
+        setStopped(false);
     };
-  
+
     const resetStopwatch = () => {
         if (timerRef.current) {
             clearInterval(timerRef.current);
             timerRef.current = null;
         }
         setRunning(false);
+        setPaused(false);
+        setStopped(true);
         setStopwatchTime(0);
     };
-  
+
     const formatTime = (time: number) => {
         const getSeconds = `0${time % 60}`.slice(-2);
         const minutes = Math.floor(time / 60);
@@ -76,86 +84,96 @@ const StopwatchTimer = () => {
     }, [stopwatchTime]);
   
     return (
-        <View style={{
+        <View onLayout={handleLayout} style={{
+                width: '100%',
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
-                width: 'auto',
-                marginTop: 40
-        }}>
-            <View onLayout={handleLayout} style={{
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignContent: 'center',
-                    marginBottom: 40
             }}>
-                <Text style={{
-                    fontSize: 60,
-                    color: themeColors.text
+            <Text style={{
+                fontSize: 60,
+                color: themeColors.text
                 }}>{formatTime(stopwatchTime)}</Text>
+            <View style={{
+                flexDirection: 'row'
+                }}>
                 <View style={{
-                    flexDirection: 'row'
-                }}>
-                    <View style={{
-                        borderBottomColor: themeColors.tint,
-                        borderBottomWidth: 3,
-                        width: getWidthInPixels()
-                    }}></View>
-                    <View style={{
-                        borderBottomColor: themeColors.subtleText,
-                        borderBottomWidth: 3,
-                        width: getAntiWidthInPixels()
-                    }}></View>
-                </View>
+                    borderBottomColor: themeColors.tint,
+                    borderBottomWidth: 3,
+                    width: getWidthInPixels()
+                }}></View>
+                <View style={{
+                    borderBottomColor: themeColors.subtleText,
+                    borderBottomWidth: 3,
+                    width: getAntiWidthInPixels()
+                }}></View>
             </View>
-            <Pressable onPress={() => startStopwatch()}
-                style={{
-                    width: '40%',
-                    height: 40,
-                    borderRadius: 8,
-                    paddingHorizontal: 10,
-                    backgroundColor: themeColors.tint,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginBottom: 12
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: '100%',
+                marginTop: 20
                 }}>
-                <Text style={{
-                    fontSize: 14,
-                    color: themeColors.text,
-                }}>Start</Text>     
-            </Pressable>
-            <Pressable onPress={() => stopStopwatch()}
-                style={{
-                    width: '40%',
-                    height: 40,
-                    borderRadius: 8,
-                    paddingHorizontal: 10,
-                    backgroundColor: themeColors.tint,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginBottom: 12
-            }}>
-                <Text style={{
-                    fontSize: 14,
-                    color: themeColors.text,
-                }}>Pause</Text>   
-            </Pressable>
-            <Pressable onPress={() => resetStopwatch()}
-                style={{
-                    width: '40%',
-                    height: 40,
-                    borderRadius: 8,
-                    paddingHorizontal: 10,
-                    backgroundColor: themeColors.tint,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginBottom: 12
-            }}>
-                <Text style={{
-                    fontSize: 14,
-                    color: themeColors.text,
-                }}>Reset</Text>   
-            </Pressable>
+                { stopped || paused ? (
+                    <Pressable onPress={() => startStopwatch()}
+                        style={{
+                            flex: 1,
+                            height: 40,
+                            borderRadius: 8,
+                            paddingHorizontal: 10,
+                            backgroundColor: themeColors.tint,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginRight: paused ? 12 : 0
+                        }}>
+                        <Text style={{
+                            fontSize: 14,
+                            color: themeColors.text,
+                        }}>Start</Text>
+                    </Pressable>
+                ) : (
+                    <View></View>
+                )}
+                { paused ? (
+                    <Pressable onPress={() => resetStopwatch()}
+                        style={{
+                            flex: 1,
+                            height: 40,
+                            borderRadius: 8,
+                            paddingHorizontal: 10,
+                            backgroundColor: themeColors.tint,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                    }}>
+                        <Text style={{
+                            fontSize: 14,
+                            color: themeColors.text,
+                        }}>Reset</Text>   
+                    </Pressable>
+                ) : (
+                    <View></View>
+                )}
+                { running ? (
+                    <Pressable onPress={() => pauseStopwatch()}
+                        style={{
+                            flex: 1,
+                            width: '100%',
+                            height: 40,
+                            borderRadius: 8,
+                            paddingHorizontal: 10,
+                            backgroundColor: themeColors.tint,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <Text style={{
+                            fontSize: 14,
+                            color: themeColors.text,
+                        }}>Pause</Text> 
+                    </Pressable>
+                ) : (
+                    <View></View>
+                )}
+            </View>
         </View>
     )
 }
