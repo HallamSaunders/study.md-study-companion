8a2026b92c3c2696869a3e933d9c0405a4749f1b
HallamSaunders <hals4072@gmail.com>
2024-09-23 15:11:31 +0100

    Adding skills page.

--- study-companion/app/(tabs)/Skills.tsx
+++ study-companion/app/(tabs)/Skills.tsx
@@ -1,10 +1,81 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+
+//Color schemes and insets
+import { useColorScheme } from '../../components/useColorScheme';
+import Colors from '../../constants/Colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Feather } from '@expo/vector-icons';
 
 const Skills = () => {
+  const [skills, setSkills] = useState<string[]>([]);
+
+  //Spacing
+  const insets = useSafeAreaInsets();
+
+  //Color schemes
+  const colorScheme = useColorScheme();
+  const themeColors = colorScheme === 'dark' ? Colors.dark : Colors.light;
+  
+  //Handle adding skill
+  const addSkill = () => {
+    setSkills([...skills, 'Skill']);
+  }
+
   return (
-    <View>
-      <Text>Skills</Text>
+    <View style={{
+      backgroundColor: themeColors.background,
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+      height: "100%",
+      width: "100%",
+    }}>
+      <ScrollView>
+        <View style={{
+          margin: 12,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignContent: 'center',
+          paddingLeft: 10,
+          paddingRight: 10
+        }}>
+            <Text style={{
+              fontSize: 20,
+              color: themeColors.text
+            }}>Your Skills</Text>
+            <Pressable onPress={() => addSkill()}
+                style={{
+                    justifyContent: 'flex-end'
+                }}>
+                <Feather name='plus' size={18} color={themeColors.text}/>
+            </Pressable>
+        </View>
+        <View style={{
+          flex: 1,
+          height: '100%',
+          justifyContent: 'flex-start',
+
+        }}>
+          <View style={{
+            width: '75%',
+            alignItems: 'center',
+            //borderWidth: 2,
+            //borderColor: 'red'
+          }}>
+            {skills.map((skill, index) => (
+              <Text key={index} style={{
+                fontSize: 14,
+                color: themeColors.text,
+                marginBottom: 8,
+              }}>
+                {skill}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
